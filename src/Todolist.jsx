3f33114db8e8e7f2e0c64ace3a3b89dcab7e853a
Parvathy36 <159471840+Parvathy36@users.noreply.github.com>
2025-04-09 22:33:28 +0530

    Update Todolist.jsx

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -6,34 +6,45 @@ function App() {
 
   // Fetch tasks
   useEffect(() => {
-    fetch('http://localhost:3000/api/tasks')
+    fetch('http://localhost:3001/api/tasks')
       .then(res => res.json())
       .then(data => setTasks(data));
   }, []);
 
   // Add task
-  const addTask = () => {
-    fetch('http://localhost:3000/api/tasks', {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3001/api/tasks', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ text: input, completed: false })
+      body: JSON.stringify({ text: input })
+    })
+    .then(() => window.location.reload());
+  };
+
+  const deleteTask = (id) => {
+    fetch(`http://localhost:3001/api/tasks/${id}`, {
+      method: 'DELETE'
     })
     .then(() => {
-      setInput('');
-      fetch('http://localhost:3000/api/tasks')
-        .then(res => res.json())
-        .then(data => setTasks(data));
+      setTasks(tasks.filter(task => task._id !== id)); // Remove from UI
     });
   };
 
   return (
     <div>
       <h1>Todo List</h1>
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={addTask}>Add</button>
+      <form onSubmit={handleSubmit}>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Add</button>
+      </form>
       <ul>
         {tasks.map(task => (
-          <li key={task._id}>{task.text}</li>
+          <li key={task._id}>
+            {task.text}
+
+            <button onClick={() => deleteTask(task._id)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
