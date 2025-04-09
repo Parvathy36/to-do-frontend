103258ae0571b70e5bb99d968c5cd0131cd3c2df
Parvathy36 <159471840+Parvathy36@users.noreply.github.com>
2025-04-09 20:48:18 +0530

    Update Todolist.jsx

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -16,19 +16,20 @@ function Todolist() {
         }
     };
 
-    return(
+    return (
         <div className="contain">
             <h1>Todo List</h1>
             <form onSubmit={handleSubmit} className="insertbox">
                 <input
-                type="text"
-                value={todoText}
-                onChange={handleInputChange}
-                placeholder="Enter a new todo"
+                    type="text"
+                    value={todoText}
+                    onChange={handleInputChange}
+                    placeholder="Enter a new todo"
                 />
                 <button type="submit">Insert</button>
             </form>
         </div>
     );
 }
+
 export default Todolist;
